cec109f2cc0540cea8a948bb230050991da40e47
SpotMatch <76890200+SpotMatch@users.noreply.github.com>
2021-01-04 18:28:59 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/10ppEeoftrEZZz6GZDgjnNaKpK1rrm9H9OJmfpQiZJlM/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyArkvY9bkZprvBfknKoM8YHqFIP-kSraO4';
