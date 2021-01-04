3016a53fabbf0e2611e98c6c6ad4dfeef3a5c8a3
SpotMatch <76890200+SpotMatch@users.noreply.github.com>
2021-01-04 18:37:32 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,5 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/10ppEeoftrEZZz6GZDgjnNaKpK1rrm9H9OJmfpQiZJlM/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
+// HIER AUF GITHUB IST DER BEISPIEL-KEY IN VERWENDUNG – UNBEDINGT BEIM UMZUG IN EIGENEN KEY UMWANDELN!
 var googleApiKey = 'AIzaSyArkvY9bkZprvBfknKoM8YHqFIP-kSraO4';
