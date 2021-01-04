13d8d8eb5b89d40336a062031dc68da9e97fd85c
SpotMatch <76890200+SpotMatch@users.noreply.github.com>
2021-01-04 16:33:25 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1mW7uBmv3pn8iwLzg9H8C7SqtTrtLWl__C2N0jJ_F_0k/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10ppEeoftrEZZz6GZDgjnNaKpK1rrm9H9OJmfpQiZJlM/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'IuFAcWvWnBVQYpzSOLuU2oBN';
