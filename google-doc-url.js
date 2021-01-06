e7232742ac2009f73bdd30f20fddda0b3576a1b1
SpotMatch <76890200+SpotMatch@users.noreply.github.com>
2021-01-06 14:00:25 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PBOGfYdnxelvHoVVL5Gt1837yOh_RRuvPlTF5mVhEJQ/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
 
 // AUF GITHUB NUR DEN BEISPIEL-KEY VERWENDEN ->  AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94  <–  BEIM UMZUG AUF EIGENEN SERVER UNBEDINGT DEN EIGENEN KEY VERWENDEN!
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
